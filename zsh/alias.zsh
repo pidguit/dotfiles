969812e1d9ba5048f4a74d5d88b69ddd36085870
Pidguit <pidguit@gmail.com>
2025-05-27 14:44:54 -0400

    Added .zsh for binds and alias and disabled fzf,don't know how it works tbh

---
+++ zsh/alias.zsh
@@ -0,0 +1,12 @@
+#  This file contains all my custom aliases
+
+# Enable color output for ls and grep
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+
+# Some extras I use
+alias neofetch='fastfetch'
+alias refresh='source ~/.zshrc'
+alias zshedit='nvim ~/.config/zsh/zshrc'
+alias update='sudo pacman -Syu;flatpak update'
+alias shutdown=poweroff
--- zsh/binds.zsh
+++ zsh/binds.zsh
@@ -1,4 +1,5 @@
 # This file contains all of my binds
+
 bindkey '^J' down-line-or-history
 bindkey '^K' up-line-or-history 
 bindkey '^H' backward-char 
