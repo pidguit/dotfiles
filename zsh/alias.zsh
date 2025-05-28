286e70d0096f931ca08cf30adca278e0292b7c25
Pidguit <pidguit@gmail.com>
2025-05-27 21:09:59 -0400

    added more git aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -10,3 +10,12 @@ alias refresh='source ~/.zshrc'
 alias zshedit='nvim ~/.config/zsh/zshrc'
 alias update='sudo pacman -Syu;flatpak update'
 alias shutdown=poweroff
+
+# git alias's, most borrowed from the ohmyzsh git plugin
+alias grt='cd "$(git rev-parse --show-toplevel || echo .)"'
+alias g='git'
+alias ga='git add'
+alias gaa='git add -A'
+alias gcm='git commit -m'
+alias gcam='git commit -a -m'
+alias gst='git status'
